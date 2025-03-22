83810363c7f57a269dbaecfd549d5fe5f3921127
Masta7200 <131053603+Masta7200@users.noreply.github.com>
2025-03-22 06:10:59 -0700

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,20 +1,19 @@
 function UserProfile() {
-    return (
-      <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center transition-shadow duration-300 hover:shadow-xl">
-        <img 
-          src="https://via.placeholder.com/150" 
-          alt="User" 
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
-        />
-        <h1 className="text-lg sm:text-xl text-blue-800 my-4 transition-colors duration-300 hover:text-blue-500">
-          John Doe
-        </h1>
-        <p className="text-sm sm:text-base text-gray-600">
-          Developer at Example Co. Loves to write code and explore new technologies.
-        </p>
-      </div>
-    );
-  }
-  
-  export default UserProfile;
-  
+  return (
+    <div className="bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center transition-shadow duration-300 hover:shadow-xl">
+      <img 
+        src="https://via.placeholder.com/150" 
+        alt="User" 
+        className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto transition-transform duration-300 ease-in-out hover:scale-110"
+      />
+      <h1 className="text-lg sm:text-xl text-blue-800 my-4 transition-colors duration-300 hover:text-blue-500">
+        John Doe
+      </h1>
+      <p className="text-sm sm:text-base text-gray-600">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
